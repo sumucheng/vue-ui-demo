b9e96566554e696181e6541ca9d24b8000c0d5d2
kiki <772765729@qq.com>
2019-11-06 18:57:28 +0800

    travis

--- src/button.vue
+++ src/button.vue
@@ -5,12 +5,16 @@
         <div class="content">
             <slot></slot>
         </div>
-    </button>
+    </button>k
 </template>
 
 <script>
+    import Icon from './icon'
     export default {
         // props: ['icon', 'iconPosition']
+        components:{
+            'g-icon':Icon
+        },
         props: {
             icon: {},
             loading: {
