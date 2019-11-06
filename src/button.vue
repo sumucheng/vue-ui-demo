9e792220926887a20b469cd376afc97160883f05
kiki <772765729@qq.com>
2019-11-06 12:41:16 +0800

    新增icon.vue

--- src/button.vue
+++ src/button.vue
@@ -1,9 +1,26 @@
 <template>
-    <button class='g-button'>提交</button>
+    <button class='g-button' v-bind:class="{[`icon-${iconPosition}`]:true}">
+        <g-icon class="icon" :name="icon" v-if="icon"></g-icon>
+        <div class="content">
+            <slot></slot>
+        </div>
+    </button>
 </template>
 
 <script>
-     export default {}
+    export default {
+        // props: ['icon', 'iconPosition']
+        props: {
+            icon: {},
+            iconPosition: {
+                type: String,
+                default: 'left',
+                validator(value) { //属性检查器
+                    return !(value !== 'left' && value !== 'right');
+                }
+            }
+        }
+    }
 </script>
 
 <style lang="scss">
@@ -14,6 +31,10 @@
         border-radius: var(--border-radius);
         border: 1px solid var(--border-color);
         background: var(--button-bg);
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
+        vertical-align: middle;
 
         &:hover {
             border-color: var(--border-color-hover);
@@ -25,6 +46,27 @@
 
         &:focus {
             outline: none;
+        }
+
+        > .icon {
+            order: 1;
+            margin-right: .2em;
+        }
+
+        > .content {
+            order: 2;
+        }
+
+        &.icon-right {
+            > .icon {
+                order: 2;
+                margin-right: 0;
+                margin-left: 0.2em;
+            }
+
+            > .content {
+                order: 1;
+            }
         }
     }
 </style>
